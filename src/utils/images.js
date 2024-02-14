2238d17b3485ef185630814c006310a8035570de
Kevinfex <kevinlepoma@gmail.com>
2024-02-13 23:48:19 -0500

    second commit

--- src/utils/images.js
+++ src/utils/images.js
@@ -4,7 +4,7 @@ export const routesImages = {
     isYes: 'https://media.tenor.com/8N58zR-v77kAAAAj/love-you.gif',
     Cr7: 'https://media1.tenor.com/m/SqkGq8p64qkAAAAC/looking-at-you-justin-bieber.gif',
     Messi: 'https://media1.tenor.com/m/LsZdWkgUXKQAAAAd/cnco-beso.gif',
-    Goku: 'https://imgmedia.libero.pe/652x359/libero/original/2021/10/12/6165ac0c3c921c4bb53e85e3.webp',
+    Goku: 'https://www.icegif.com/wp-content/uploads/2022/09/icegif-803.gif',
     Home2: 'https://cdn.pixabay.com/animation/2022/09/28/02/42/02-42-52-631_512.gif'
 }
 

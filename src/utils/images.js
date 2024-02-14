6c2cf0010f23e3143c99e3f0584cdcebe95aec01
Kevinfex <kevinlepoma@gmail.com>
2024-02-13 23:32:53 -0500

    first commit

---
+++ src/utils/images.js
@@ -0,0 +1,11 @@
+export const routesImages = {
+    Home: 'https://i.pinimg.com/originals/f6/a5/f7/f6a5f7ddff1f05cbcc560256b9f98c2e.gif',
+    isNot: 'https://media.tenor.com/ivKWdfdbV3EAAAAi/goma-goma-cat.gif',
+    isYes: 'https://media.tenor.com/8N58zR-v77kAAAAj/love-you.gif',
+    Cr7: 'https://media1.tenor.com/m/SqkGq8p64qkAAAAC/looking-at-you-justin-bieber.gif',
+    Messi: 'https://media1.tenor.com/m/LsZdWkgUXKQAAAAd/cnco-beso.gif',
+    Goku: 'https://imgmedia.libero.pe/652x359/libero/original/2021/10/12/6165ac0c3c921c4bb53e85e3.webp',
+    Home2: 'https://cdn.pixabay.com/animation/2022/09/28/02/42/02-42-52-631_512.gif'
+}
+
+export const options = ['¿Segura?', 'Segurisima?', 'Estas completamente segura?', 'Ni por Justin Bieber?', 'Ni por CNCO?', 'No te arrepentiras?', 'Pero si estas segura?', 'En serio no 😐?', 'Justin Bieber está decepcionado', 'Luffy también', 'Ñachu!!', 'Ñachu!! di que sii', 'Chauchilla 🤗 di que si']
